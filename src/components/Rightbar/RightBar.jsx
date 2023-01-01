c72cbbe87c946a653c37fe1625f1918efcd821fb
FedeCraviotto <fcraviotto.dev@gmail.com>
2023-01-01 18:36:04 -0300

    Commencing rightBar

--- src/components/Rightbar/RightBar.jsx
+++ src/components/Rightbar/RightBar.jsx
@@ -1,8 +1,34 @@
 import './rightBar.scss';
+import userPhoto from '../../assets/images/users/images/guitarrist-playing.jpg';
 function RightBar(){
     return(
         <div className='rightBar'>
-            RIghtbar
+            <div className="container">
+                <div className="item">
+                    <span>Suggestions</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={userPhoto} alt="UserPhoto" />
+                            <span>Suggested user1</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>    
+                
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={userPhoto} alt="UserPhoto" />
+                            <span>Suggested user2</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
